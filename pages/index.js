e9bbafc61b8817a67a1d7228bed8fc860c4d59a9
warren yun <xxwarrenhdxx@gmail.com>
2021-07-29 10:47:14 -0400

    Format for mobile

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,23 @@ import homeStyles from '../styles/Home.module.css';
 export default function Home () {
     return (
         <div className={homeStyles.container}>
-            <div className={homeStyles.desc}>
-                <h1 className={homeStyles.title}>The Orpheus Show</h1>
-                <div>
-                    <h3 className={homeStyles.subtitle}>
-                        Made by The Hack Club Podcast Team 
-                        <br />
-                        Hosted on Transistor.fm
-                    </h3>
+            <div className={homeStyles.left}>
+
+                <div className={homeStyles.desc}>
+                    <h1 className={homeStyles.title}>The Orpheus Show</h1>
+                    <div>
+                        <h3 className={homeStyles.subtitle}>
+                            Made by The Hack Club Podcast Team 
+                            <br />
+                            Hosted on Transistor.fm
+                        </h3>
+                    </div>
                 </div>
+
             </div>
+            <div className={homeStyles.right}>
             
+            </div>
         </div>
     );
 }
