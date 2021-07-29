7db68bfa92fa51f697277ff551a4cffbeb9657b6
warren yun <xxwarrenhdxx@gmail.com>
2021-07-28 23:29:19 -0400

    Start structuring index page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home () {
+    return (
+        <div className="container">
+            <h1 className="title">
+                <strong>The Orpheus Show</strong>
+            </h1>
+            <h3 className="subtitle">
+                Made by The Hack Club Podcast Team 
+                <br />
+                Hosted on Transistor.fm
+            </h3>
+        </div>
+    );
+}
