945e451f258ff966298ee9f8f3f7a6d9551835c7
warren yun <xxwarrenhdxx@gmail.com>
2021-07-29 00:01:55 -0400

    Add padding

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import homeStyles from '../styles/Home.module.css';
+
 export default function Home () {
     return (
-        <div className="container">
-            <h1 className="title">
+        <div className={homeStyles.container}>
+            <h1 className={homeStyles.title}>
                 <strong>The Orpheus Show</strong>
             </h1>
-            <h3 className="subtitle">
+            <h3 className={homeStyles.subtitle}>
                 Made by The Hack Club Podcast Team 
                 <br />
                 Hosted on Transistor.fm
