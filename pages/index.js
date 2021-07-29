072b1a76cb2b046348607dbbc0d009a6d7633621
warren yun <xxwarrenhdxx@gmail.com>
2021-07-29 09:46:11 -0400

    Position intro and desc

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,17 @@ import homeStyles from '../styles/Home.module.css';
 export default function Home () {
     return (
         <div className={homeStyles.container}>
-            <h1 className={homeStyles.title}>
-                <strong>The Orpheus Show</strong>
-            </h1>
-            <h3 className={homeStyles.subtitle}>
-                Made by The Hack Club Podcast Team 
-                <br />
-                Hosted on Transistor.fm
-            </h3>
+            <div className={homeStyles.desc}>
+                <h1 className={homeStyles.title}>The Orpheus Show</h1>
+                <div>
+                    <h3 className={homeStyles.subtitle}>
+                        Made by The Hack Club Podcast Team 
+                        <br />
+                        Hosted on Transistor.fm
+                    </h3>
+                </div>
+            </div>
+            
         </div>
     );
 }
