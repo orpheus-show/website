43cb150e3642c00e95b1e9249cfb2c537398e3e2
Arcade Wise <l3gacy.b3ta@disroot.org>
2021-07-29 22:35:00 -0400

    Merge branch 'main' of https://github.com/Orpheuspod/site

--- components/dropdown.js
+++ components/dropdown.js
@@ -9,14 +9,23 @@ export default function Dropdown() {
     }
     return (
         <div className={styles.button}>
-            <button className={styles.btn} onClick={toggle}>Toggle Subjects</button>
+            <button className={styles.btn} onClick={toggle}>Listen Now</button>
             {/*The bottom code should toggle on and off when the button is pressed*/}
             <div
+                onClick={toggle}
                 className={styles.div}
                 style={{
                     display: showMe ? "block" : "none"
                 }}>
-                This should toggle my display
+                <a onClick={toggle} target="_blank" href="https://open.spotify.com/show/3q6wJccR9gjQZgOjr23PEJ">Spotify</a>
+                <hr />
+                {/*<a onClick={toggle}>Apple Podcasts</a>*/}
+                <a onClick={toggle} target="_blank" href="https://music.amazon.com/podcasts/78453879-9d50-4a49-a11e-436f9cb277a2/the-orpheus-show">Amazon Music</a>
+                <hr />
+                <a onClick={toggle} target="_blank" href="https://www.breaker.audio/the-orpheus-show">Breaker</a>
+                <hr />
+                <a onClick={toggle} target="_blank" href="https://feeds.transistor.fm/the-orpheus-show">RSS Feed</a>
+
             </div>
         </div>
     );
