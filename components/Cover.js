5dec7f015f51d73a39f9c82942046687cb81f9cc
Arcade Wise <l3gacy.b3ta@disroot.org>
2021-07-30 15:00:47 -0400

    Change cover to a nextjs Image, for better loading, and just better stuff in general.

--- components/Cover.js
+++ components/Cover.js
@@ -1,11 +1,18 @@
 import homeStyles from '../styles/Home.module.css';
 import { useColorMode } from "theme-ui"
+import Image from "next/image";
 
 export default function Cover() {
     const [mode] = useColorMode()
     const isDark = mode === "dark"
     return (
-        <img 
+        <Image
+            height = "450px"
+            width = "450px"
+
+            layout="responsive"
+            placeholder="empty"
+
             src={
                 isDark
                 ? "/cover-dark.png"
