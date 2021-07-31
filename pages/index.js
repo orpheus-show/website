b6ba6a02b910546c708a1576f69400e013e83da5
Arav Narula <proteximportsexports@gmail.com>
2021-07-31 10:39:22 -0400

    Update index.js
      <div className={episodeStyles.container}>
                        <h1>Episodes</h1>
                        <h3>Check out the latest episodes from The Orpheus Show!</h3>
                    </div>

--- pages/index.js
+++ pages/index.js
@@ -53,10 +53,7 @@ export default function Home () {
                     </div>
                 </div>
                 <Team />
-                <div className={episodeStyles.container}>
-                    <h1>Episodes</h1>
-                    <h3>Check out the latest episodes from The Orpheus Show!</h3>
-                </div>
+              
             </div>
             <Footer />
 
