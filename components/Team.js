c5604cf6558eea4cd1c607ee6510216144367bca
warren yun <xxwarrenhdxx@gmail.com>
2021-07-30 23:22:18 -0400

    Add arav and format list between members

--- components/Team.js
+++ components/Team.js
@@ -4,8 +4,16 @@ const Team = () => {
     return (
         <div className={teamStyles.container}>
             <h1>Team</h1>
-            <h3>Thanks to our amazing team members that made The Orpheus Show possible!</h3>
+            <h3 className={teamStyles.subtitle}>Thanks to our amazing team members that made The Orpheus Show possible!</h3>
             <ul className={teamStyles.teamList}>
+                <li className={teamStyles.teamMember}>
+                    <img src="/team-members/arav.jfif" className={teamStyles.teamPFP}/>
+                    <h3>
+                        Arav
+                        <br />
+                        Host
+                    </h3>
+                </li>
                 <li className={teamStyles.teamMember}>
                     <img src="/team-members/warren.png" className={teamStyles.teamPFP}/>
                     <h3>
