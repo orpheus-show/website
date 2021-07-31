e388003cd46e9dbcb54df05ceb0e41c42043b913
Arav Narula <proteximportsexports@gmail.com>
2021-07-31 10:43:22 -0400

    Update Episode.js

--- components/Episode.js
+++ components/Episode.js
@@ -2,7 +2,10 @@ import styles from '../styles/Episode.module.css';
 
 const Episode = () => {
     return (
-        <div className={styles.instance}>
+         <div className={episodeStyles.container}>
+                    <h1>Episodes</h1>
+                    <h3>Check out the latest episodes from The Orpheus Show!</h3>
+                </div>
             
         </div>
     )
