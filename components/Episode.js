1b0e8302a8d20449f0c6e36520890b30a8282618
warren yun <xxwarrenhdxx@gmail.com>
2021-07-31 16:15:26 -0400

    Add details prop

--- components/Episode.js
+++ components/Episode.js
@@ -1,11 +1,16 @@
 import episodeStyles from '../styles/Episode.module.css';
 
-const Episode = () => {
+const Episode = ({details}) => {
     return (  
         <div className={episodeStyles.container}>
-            <img src="" className={episodeStyles.episodePFP}/>
+            <img src={details.img} className={episodeStyles.episodePFP}/>
             <div className={episodeStyles.info}>
-                
+                <h3 className={episodeStyles.name}>
+                    {details.name}
+                </h3>
+                <h4 className={episodeStyles.date}>
+                    {details.date}
+                </h4>
             </div>
         </div>
     )
