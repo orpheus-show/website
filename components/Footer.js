85bcf0b5f269c68ab13eef0a8b4bed146e01c6d2
Arcade Wise <l3gacy.b3ta@disroot.org>
2021-07-31 15:04:03 -0400

    my god links

--- components/Footer.js
+++ components/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
             <h2 className={footerStyles.title}>The Orpheus Show</h2>
             <ul className={footerStyles.social}>
                 <li>
-                    <a href="https://github.com/Orpheuspod" target="_blank">
+                    <a rel="noreferrer" href="https://github.com/Orpheuspod" target="_blank">
                         <Image width={"30pt"} height={"30pt"} alt="github logo" src="/github.svg" className={footerStyles.socialItem} />
                     </a>
                 </li>
             </ul>
             <h3>This Website/Podcast is Lisenced in Apache-2.0, and is not affilated with Hack Club or endorsed by Hack Club.</h3>
-            <h4>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" target="_blank">Dave Gandy</a> from <a href="https://www.flaticon.com/">Flaticon</a></h4>
+            <h4>Icons made by <a rel="noreferrer" href="https://www.flaticon.com/authors/dave-gandy" target="_blank">Dave Gandy</a> from <a rel="noreferrer" href="https://www.flaticon.com/">Flaticon</a></h4>
             
         </div>
     )
