0816c3063c4c95b7c20b00ec08ca1b74f3f2c473
Arav Narula <proteximportsexports@gmail.com>
2021-07-31 10:48:45 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home () {
                     </div>
                 </div>
                 <Team />
-              <Episode />
+
                                 
             </div>
             <Footer />
