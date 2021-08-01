7d6c9bf6b4f23833a2a6a3e74236744a3db30328
warren yun <xxwarrenhdxx@gmail.com>
2021-08-01 09:32:17 -0400

    Make background look a bit less invading + change font color to make it stand out

--- pages/index.js
+++ pages/index.js
@@ -51,11 +51,17 @@ export default function Home (props) {
                         </div>
                     </div>
                 </div>
-                <Team />
-                <div className={episodeStyles.container}>
-                    <h1>Episodes</h1>
-                    <h3>Check out the latest episodes from The Orpheus Show!</h3>
+
+                <div className={homeStyles.bottom}>
+                    <Team />
+
+                    <div className={episodeStyles.container}>
+                        <h1>Episodes</h1>
+                        <h3>Check out the latest episodes from The Orpheus Show!</h3>
+                    </div>
+
                 </div>
+
             </div>
             <Footer />
 
