d922f5cc85f84e133adde209b91eb5ec2925a9fd
Narulacodes <>
2021-08-01 15:06:03 +0000

    Add animation from team image to icons

--- components/Footer.js
+++ components/Footer.js
@@ -6,17 +6,17 @@ const Footer = () => {
         <div className={footerStyles.container}>
             <h2 className={footerStyles.title}>The Orpheus Show</h2>
             <ul className={footerStyles.social}>
-                <li>
+                <li className={footerStyles.icons}>
                     <a rel="noreferrer" href="https://github.com/Orpheuspod" target="_blank">
                         <Icon glyph="github" size={64} />
                     </a>
                 </li>
-                    <li>
+                    <li className={footerStyles.icons}>
                     <a rel="noreferrer" href="https://app.slack.com/client/T0266FRGM/C029KLH2UCU" target="_blank">
                         <Icon glyph="slack" size={64} />
                     </a>
                 </li>
-                <li>
+                <li className={footerStyles.icons} >
                     <a rel="noreferrer" href="https://www.youtube.com/channel/UCTES1rgjcKBjSUSJRpyLPzA" target="_blank">
                         <Icon glyph="youtube" size={64} />
                     </a>
