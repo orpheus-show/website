2dc244e67834ceceab5d35df40c8f7b27b469e62
Narulacodes <>
2021-08-01 14:52:56 +0000

    replace icon with Supercon Github

--- components/Footer.js
+++ components/Footer.js
@@ -8,13 +8,16 @@ const Footer = () => {
             <ul className={footerStyles.social}>
                 <li>
                     <a rel="noreferrer" href="https://github.com/Orpheuspod" target="_blank">
-                        <Image width={"30pt"} height={"30pt"} alt="github logo" src="/github.svg" className={footerStyles.socialItem} />
+                        <Icon glyph="github-fill" size={55} />
                     </a>
                 </li>
             </ul>
             <h3>This Website/Podcast is Lisenced in Apache-2.0, and is not affilated with Hack Club or endorsed by Hack Club.</h3>
             <h4>Icons made by <a rel="noreferrer" href="https://www.flaticon.com/authors/dave-gandy" target="_blank">Dave Gandy</a> from <a rel="noreferrer" href="https://www.flaticon.com/">Flaticon</a></h4>
-            
+             <div style={{ color: 'magenta' }}>
+    
+   
+  </div>
         </div>
     )
 }
