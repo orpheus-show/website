77914117820b0623a3802e35a2599800fda9a7d3
warren yun <xxwarrenhdxx@gmail.com>
2021-08-01 23:56:48 -0400

    Have button redirect to song page

--- components/Episode.js
+++ components/Episode.js
@@ -2,17 +2,21 @@ import episodeStyles from '../styles/Episode.module.css';
 
 function Episode({details}) {
     return (  
-        <div className={episodeStyles.container}>
-            <img src={details.images[1].url} className={episodeStyles.episodeThumbnail} />
-            <div className={episodeStyles.info}>
-                <h3 className={episodeStyles.name}>
-                    {details.name}
-                </h3>
-                <h4 className={episodeStyles.date}>
-                    {details.release_date}
-                </h4>
-            </div>
-        </div>
+        <button className={episodeStyles.btn}>
+            <a href={details.external_urls.spotify} target="_blank">
+                <div className={episodeStyles.container}>
+                    <img src={details.images[1].url} className={episodeStyles.episodeThumbnail} />
+                    <div className={episodeStyles.info}>
+                        <h3 className={episodeStyles.name}>
+                            {details.name}
+                        </h3>
+                        <h4 className={episodeStyles.date}>
+                            {details.release_date}
+                        </h4>
+                    </div>
+                </div>
+            </a>
+        </button>
     )
 }
 
