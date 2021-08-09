8bad403e80476fb3590909833492b7d99e58973c
Asa Paparo <77820164+AngleSideAngle@users.noreply.github.com>
2021-08-09 15:08:41 -0400

    added links for new members

--- components/Team.js
+++ components/Team.js
@@ -43,19 +43,19 @@ const members = [
     {
         "name": "Hugo",
         "desc": "Marketing",
-        "link": "https://podcast.hackclub.com",
+        "link": "http://twitter.com/rpitechguy",
         "image": "corgi.png"
     },
     {
         "name": "Rebecca",
         "desc": "Operations",
-        "link": "https://podcast.hackclub.com",
+        "link": "https://rbccawang.com/",
         "image": "placeholder.png"
     },
     {
         "name": "Jason",
         "desc": "Production",
-        "link": "https://podcast.hackclub.com",
+        "link": "https://jasonaa.me/",
         "image": "placeholder.png"
     }
 ]
