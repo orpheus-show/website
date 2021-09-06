37b156b9d33e8cb8ed73a9f4dfd2b65398a8e5dc
Arav Narula <proteximportsexports@gmail.com>
2021-09-06 15:57:39 -0400

    [prob broke something] Make it go to new notes page

--- components/buttons/ViewNotes.js
+++ components/buttons/ViewNotes.js
@@ -9,35 +9,10 @@ export default function ViewNotes() {
   return (
     <div className={styles.button}>
       <button className={styles.btn} onClick={toggle}>
+        <a href="/notes"> 
         View the Notes
+        </a>
       </button>
-      {/*The bottom code should toggle on and off when the button is pressed*/}
-      <div
-        onClick={toggle}
-        className={styles.div}
-        style={{
-          display: showMe ? 'block' : 'none'
-        }}
-      >
-        <a
-          rel="noreferrer"
-          onClick={toggle}
-          target="_blank"
-          href="https://www.notion.so/devenjadhav/Max-Interview-Details-be41671482e349abbc73e69965dd44b9"
-        >
-          Read about Max's Interview
-        </a>
-        <hr />
-        {/*<a rel="noreferrer" onClick={toggle}>View ZRL - Interview </a>*/}
-        <a
-          rel="noreferrer"
-          onClick={toggle}
-          target="_blank"
-          href="https://www.notion.so/devenjadhav/The-Orpheus-Show-faa883027b5849e9af8e9d3b349d74ad"
-        >
-          View the entire Notion
-        </a>
-      </div>
     </div>
   )
 }
