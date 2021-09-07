166357d324352cd9bb385b41ca1d89b22e3d27e4
Arash <47273556+arashnrim@users.noreply.github.com>
2021-09-07 11:13:05 +0800

    Clean up notes button

--- components/buttons/ViewNotes.js
+++ components/buttons/ViewNotes.js
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import styles from '../../styles/Button.module.css'
 
+import Link from 'next/link'
+
 export default function ViewNotes() {
-  const [showMe, setShowMe] = useState(false)
-  function toggle() {
-    setShowMe(!showMe)
-  }
   return (
     <div className={styles.button}>
-      <button className={styles.btn} onClick={toggle}>
-        <a href="/notes">View the Notes</a>
-      </button>
+      <Link href="/notes">
+        <button className={styles.btn}>View the Notes</button>
+      </Link>
     </div>
   )
 }
