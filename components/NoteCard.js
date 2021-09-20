a72967c4ddf8543d381d4cbc7f7e8a235df4ec1a
Arash <47273556+arashnrim@users.noreply.github.com>
2021-09-20 17:01:28 +0800

    Resolve linter errors

--- components/NoteCard.js
+++ components/NoteCard.js
@@ -2,7 +2,12 @@ import noteStyles from '../styles/Notes.module.css'
 
 function NoteCard({ notes }) {
   return (
-    <a href={notes.link} className={noteStyles.noteCard} target="_blank">
+    <a
+      href={notes.link}
+      className={noteStyles.noteCard}
+      target="_blank"
+      rel="noreferrer"
+    >
       <img src="/note-assets/zach_latta.png" className={noteStyles.img} />
       <div className={noteStyles.subDiv}>
         <h1 className={noteStyles.cardSub}>{notes.sub}</h1>
