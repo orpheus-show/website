7e189746d7829af812756eb829aa9c367f997152
warren yun <xxwarrenhdxx@gmail.com>
2021-09-20 08:13:27 -0400

    Add images to notes section

--- components/NoteCard.js
+++ components/NoteCard.js
@@ -3,12 +3,12 @@ import noteStyles from '../styles/Notes.module.css'
 function NoteCard({ notes }) {
   return (
     <a href={notes.link} className={noteStyles.noteCard} target="_blank">
-      <img src="/note-assets/zach_latta.png" className={noteStyles.img}/>
+      <img src={notes.image} className={noteStyles.img}/>
       <div className={noteStyles.subDiv}>
-        <h1 className={noteStyles.cardSub}>{notes.sub}</h1>
+        <h1 className={noteStyles.cardSub}>{notes.title}</h1>
       </div>
     </a>
   )
 }
 
-export default NoteCard
+export default NoteCard;
--- pages/notes.js
+++ pages/notes.js
@@ -3,24 +3,25 @@ import NoteCard from '../components/NoteCard'
 import notesStyles from '../styles/Notes.module.css'
 import Link from 'next/link'
 
+// for images, use 350x270 pixel image
 const allNotes = [
   {
     title: 'Max Wofford',
     sub: 'Episode 1',
     link: 'https://devenjadhav.notion.site/Max-Interview-Details-be41671482e349abbc73e69965dd44b9',
-    image: ''
+    image: '/note-assets/msw.png'
   },
   {
     title: 'Zach Latta',
     sub: 'Episode 2',
     link: 'https://devenjadhav.notion.site/ZRL-d54d720fe8ba410da3d1c531b0c2dae3',
-    image: ''
+    image: '/note-assets/zach_latta.png'
   },
   {
     title: 'Zach Fogg',
     sub: 'Episode 3',
     link: 'https://devenjadhav.notion.site/ZFogg-c16aacb03369415c9a7beb4c8823dd98',
-    image: ''
+    image: '/note-assets/zfogg.png'
   }
 ]
 
