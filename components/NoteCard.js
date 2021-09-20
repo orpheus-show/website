835213972f7f012f2b171b5a25a99670affda8a7
warren yun <xxwarrenhdxx@gmail.com>
2021-09-19 22:36:16 -0400

    Use cropped image

--- components/NoteCard.js
+++ components/NoteCard.js
@@ -3,9 +3,7 @@ import noteStyles from '../styles/Notes.module.css'
 function NoteCard({ notes }) {
   return (
     <a href={notes.link} className={noteStyles.noteCard} target="_blank">
-      <div className={noteStyles.titleDiv}>
-        <h1 className={noteStyles.cardTitle}>{notes.title}</h1>
-      </div>
+      <img src="/note-assets/zach_latta.png" className={noteStyles.img}/>
       <div className={noteStyles.subDiv}>
         <h1 className={noteStyles.cardSub}>{notes.sub}</h1>
       </div>
