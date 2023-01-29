791afcfcefd84e34a0cc360a5ce986b437e6d672
Arav Narula <proteximportsexports@gmail.com>
2023-01-29 13:50:39 -0500

    revert to old help heading

--- components/Team.js
+++ components/Team.js
@@ -91,7 +91,7 @@ const Team = () => {
         >
           Slack channel
         </a>{' '}
-        We're currently looking for new people to help us produce the show!
+     and find something to help with!
       </h3>
       <ul className={teamStyles.teamList}>
         {members.map((member, i) => (
