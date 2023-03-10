ff0c72f4539ce261c805b57f1341afbbb7462f41
Arav Narula <proteximportsexports@gmail.com>
2023-03-10 13:03:22 -0500

    change notes link to notion insted of the notes page

--- components/buttons/ViewNotes.js
+++ components/buttons/ViewNotes.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function ViewNotes() {
   return (
     <div className={styles.button}>
-      <Link href="/notes">
+      <Link href="https://devenjadhav.notion.site/devenjadhav/The-Orpheus-Show-faa883027b5849e9af8e9d3b349d74ad">
         <button className={styles.btn}>View the Notes</button>
       </Link>
     </div>
